c02c44f1ae3fc6449a531ca230e231d46572f99d
Игорь <applecake@inbox.ru>
2020-03-10 18:01:45 +0300

    add basic redux options

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import { AppLoading } from "expo";
+import { Provider } from "react-redux";
+import { createStore } from 'redux'
 
 import { bootstrap } from "./src/bootstrap";
 import { AppNavigation } from "./src/navigation/AppNavigation";
-
+import rootReducer from './src/store'
 
 export default function App() {
   const [isReady, setIsReady] = useState(false);
@@ -18,7 +20,11 @@ export default function App() {
     );
   }
 
+  const store = createStore(rootReducer)
+
   return (
+    <Provider store={store}>
       <AppNavigation />
+    </Provider>
   );
 }
