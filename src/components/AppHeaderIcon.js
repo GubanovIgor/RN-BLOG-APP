bec23ad0847ef7964e0c23d718803f069c923bcc
Игорь <applecake@inbox.ru>
2020-03-13 15:12:13 +0300

    change ios header styles

--- src/components/AppHeaderIcon.js
+++ src/components/AppHeaderIcon.js
@@ -1,7 +1,14 @@
 import React from "react";
-import { AntDesign } from '@expo/vector-icons';
+import { AntDesign } from "@expo/vector-icons";
 import { HeaderButton } from "react-navigation-header-buttons";
+import { Platform } from "react-native";
+import { THEME } from "../theme";
 
 export const AppHeaderIcon = props => (
-  <HeaderButton {...props} IconComponent={AntDesign} iconSize={24} color='#fff'/>
+  <HeaderButton
+    {...props}
+    IconComponent={AntDesign}
+    iconSize={24}
+    color={Platform.OS === "android" ? "#fff" : THEME.MAIN_COLOR}
+  />
 );
--- src/navigation/navigationOptions/navigationOptions.js
+++ src/navigation/navigationOptions/navigationOptions.js
@@ -1,8 +1,13 @@
 import { THEME } from "../../theme";
+import { Platform } from "react-native";
 
 export const screenDefaultOptions = {
   headerStyle: {
-    backgroundColor: Platform.OS === "android" ? THEME.MAIN_COLOR : "#fff"
+    backgroundColor: Platform.OS === "android" ? THEME.MAIN_COLOR : "#fff",
+    ...(Platform.OS === "ios" && {
+      borderBottomWidth: 2,
+      borderBottomColor: THEME.MAIN_COLOR
+    })
   },
   headerTintColor: Platform.OS === "android" ? "#fff" : THEME.MAIN_COLOR,
   headerTitleStyle: {
